030b2011c6a8829793529fac47e76b6b49dc7045
Angelinic05 <152038826+Angelinic05@users.noreply.github.com>
2024-02-15 00:01:15 -0500

    feat: :rocket: Actualizacion maquina de alimentos
    Ejercicio finalizado maquina de alimentos
    
    @CampusLands

--- main.js
+++ main.js
@@ -2,4 +2,48 @@
 // Una máquina de alimentos tiene productos de tres tipos, A, B y C, que valen respectivamente $270, $340 y $390. La máquina acepta y da de vuelto monedas de $10, $50 y $100.
 // Escriba un programa que pida al usuario elegir el producto y luego le pida ingresar las monedas hasta alcanzar el monto a pagar. Si el monto ingresado es mayor que el precio del producto, el programa debe entregar las monedas de vuelto, una por una.
 
-console.log("Angeli")
+
+const preciosProductos = {
+    A: 270,
+    B: 340,
+    C: 390
+  };
+  
+  let mensajeProductos = "Productos disponibles:\n";
+  for (let producto in preciosProductos) {
+    mensajeProductos += `${producto}: $${preciosProductos[producto]}\n`;
+  }
+  confirm(mensajeProductos);
+  const valoresMonedas = [100, 50, 10];
+  const producto = prompt("Elija un producto:").toUpperCase();
+  
+  if (!preciosProductos[producto]) {
+    confirm("No existe el producto");
+  } else {
+    const precioProducto = preciosProductos[producto];
+    let montoIngresado = 0;
+  
+    while (montoIngresado < precioProducto) {
+      const moneda = parseInt(prompt("Ingrese una moneda:"));
+  
+      if (![10, 50, 100].includes(moneda)) {
+        confirm("Moneda no válida.");
+        continue;
+      }
+  
+      montoIngresado += moneda;
+    }
+  
+    let vuelto = montoIngresado - precioProducto;
+    confirm("Su vuelto:");
+  
+    for (let valor of valoresMonedas) {
+      while (vuelto >= valor) {
+        confirm(valor);
+        vuelto -= valor;
+      }
+    }
+  }
+  
+
+  
